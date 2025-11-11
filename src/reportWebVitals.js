85d0892ccd1af9a28fcdc5c451be05a1e23544d4
Juan Villa <ing.villa.jimenez@gmail.com>
2025-11-11 17:54:03 -0600

    2-print-web-vitals

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,15 @@
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+import { onCLS, onLCP, onFCP, onTTFB, onINP } from "web-vitals";
+
+function sendToConsole(metric) {
+  console.log(metric.name, metric.value);
+}
+
+const reportWebVitals = () => {
+  onCLS(sendToConsole);
+  onLCP(sendToConsole);
+  onFCP(sendToConsole);
+  onTTFB(sendToConsole);
+  onINP(sendToConsole);
 };
 
 export default reportWebVitals;
